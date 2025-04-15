433826e9955c3f8adb58ae335514d0e77a3b1743
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2025-04-15 13:05:18 +0200

    using full duplex instead

--- config.h
+++ config.h
@@ -1,2 +1,6 @@
 #define SPLIT_HAND_PIN C13
 #define SPLIT_HAND_PIN_LOW_IS_LEFT
+
+#define SERIAL_USART_FULL_DUPLEX     // Enable full duplex operation mode.
+#define SERIAL_USART_TX_PIN      B6  // USART TX pin
+#define SERIAL_USART_RX_PIN      B7  // USART RX pin
